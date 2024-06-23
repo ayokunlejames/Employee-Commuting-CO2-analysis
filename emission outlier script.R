126f4681f7df18d0a1a7740ef84bbe91ae5fe95d
ayokunlejames <129544207+ayokunlejames@users.noreply.github.com>
2024-06-23 14:17:35 +0100

    Update emission outlier script.R

--- emission outlier script.R
+++ emission outlier script.R
@@ -37,10 +37,3 @@ outliers |> View()
 
 #save csv file
 write.csv(outliers, "outliers.csv", row.names = FALSE)
-
-ggplot(dataset, aes(y = Emission)) +
-  geom_boxplot(fill = "blue", color = "black", alpha = 0.7) +
-  geom_jitter(aes(x = 1, color = (Emission <lower_bound | Emission >upper_bound)), width = 0.2, size = 2) +
-  scale_color_manual(values = c("black", "red")) +
-  labs(title = "Boxplot of CO2 emission", y = "Emissions")+
-  theme(legend.position = "none")
